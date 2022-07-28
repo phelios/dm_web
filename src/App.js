26bb2b2ccfe5e117c3306bdee7b4e5f9b31a7883
Sufendy “Phelios” Cyros <phelio.xion@gmail.com>
2022-07-28 13:45:24 +1000

    Move spinner to nav bar

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import {
   Navigate,
   Link
 } from 'react-router-dom';
-import LayoutProvider from './components/providers/LayoutProvider';
 import Categories from './components/pages/Categories';
 import Criteria from './components/pages/Criteria';
 import ProjectCriteria from './components/pages/ProjectCriteria';
@@ -19,42 +18,57 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { LoadingProvider, useLoading } from './components/providers/LoadingContext';
+import { Spinner } from 'react-bootstrap';
 
 
 function App() {
 
   const [pageTitle, setPageTitle] = useState('');
 
+  function TbdSpinner() {
+    const {isLoading} = useLoading();
+
+    if (isLoading) {
+      return <Spinner animation='border' variant='light' />
+    } else {
+      return null
+    }
+  }
+
   return (
-    <LayoutProvider pageTitle={pageTitle}>
-      <Router>
-        <Navbar bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand as={Link} to="projects">DM</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link as={Link} to="projects">Projects</Nav.Link>
-              <Nav.Link as={Link} to="categories">Categories</Nav.Link>
-              <Nav.Link as={Link} to="criteria">Criteria</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
-        <h1>
-          <Row>
-            <Col>{pageTitle}</Col>
-          </Row>
-        </h1>
-        <Routes>
-          <Route path="/" element={<Navigate to="projects" replace={true} />} />
-          <Route path="projects" element={<Projects setPageTitle={setPageTitle} />} />
-          <Route path="projects/:projectId/criteria" element={<ProjectCriteria setPageTitle={setPageTitle} />} />
-          <Route path="projects/:projectId" element={<Project setPageTitle={setPageTitle} />} />
-          <Route path="projects/:projectId/candidate/:candidateId" element={<CandidateScore setPageTitle={setPageTitle} />} />
-          <Route path="categories" element={<Categories setPageTitle={setPageTitle} />} />
-          <Route path="criteria" element={<Criteria setPageTitle={setPageTitle} />} />
-          <Route path="*" element={<h1>404</h1>} />
-        </Routes>
-      </Router>
-    </LayoutProvider>
+    <LoadingProvider pageTitle={pageTitle}>
+      <Container>
+        <Router>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand as={Link} to="projects">DM</Navbar.Brand>
+              <Nav className="me-auto">
+                <Nav.Link as={Link} to="projects">Projects</Nav.Link>
+                <Nav.Link as={Link} to="categories">Categories</Nav.Link>
+                <Nav.Link as={Link} to="criteria">Criteria</Nav.Link>
+              </Nav>
+              <TbdSpinner />
+            </Container>
+          </Navbar>
+          <h1>
+            <Row>
+              <Col>{pageTitle}</Col>
+            </Row>
+          </h1>
+          <Routes>
+            <Route path="/" element={<Navigate to="projects" replace={true} />} />
+            <Route path="projects" element={<Projects setPageTitle={setPageTitle} />} />
+            <Route path="projects/:projectId/criteria" element={<ProjectCriteria setPageTitle={setPageTitle} />} />
+            <Route path="projects/:projectId" element={<Project setPageTitle={setPageTitle} />} />
+            <Route path="projects/:projectId/candidate/:candidateId" element={<CandidateScore setPageTitle={setPageTitle} />} />
+            <Route path="categories" element={<Categories setPageTitle={setPageTitle} />} />
+            <Route path="criteria" element={<Criteria setPageTitle={setPageTitle} />} />
+            <Route path="*" element={<h1>404</h1>} />
+          </Routes>
+        </Router>
+      </Container>
+    </LoadingProvider>
   );
 }
 
